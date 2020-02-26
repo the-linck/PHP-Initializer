240576e73a3b044b74a6f0ad068a022650e1cd26
Mauro Linck <mrlm03021994@gmail.com>
2020-02-26 10:54:40 -0300

    Adding a handy ReadOnlyProperties trait to the project Updating brackets position

--- Initializer.php
+++ Initializer.php
@@ -4,8 +4,7 @@
  * Provides a default constuctor that takes an array (if passed) to set rightaway properties values,
  * setting assigning each existing property to the value that has it's name as key.
  */
-trait ArrayInitializer
-{
+trait ArrayInitializer {
     /**
      * Initializes the object with an array, using keys as properties names.
      * This initializer allows setting every field acessible *inside* the object,
@@ -29,8 +28,7 @@ trait ArrayInitializer
  * Provides a default constuctor that takes another object (if passed) to set rightaway the properties of this object,
  * copying all values of visible properties that exist in this object.
  */
-trait ObjectInitializer
-{
+trait ObjectInitializer {
     /**
      * Initializes this object with another object's properties values, using all visible properties of given $Values
      * object.
@@ -45,6 +43,35 @@ trait ObjectInitializer
                     $this->{$Property} = $Value;
                 }
             }
+        }
+    }
+}
+
+
+
+
+/**
+ * Allows to have readonly properties in a class, as long they are not public.
+ */
+trait ReadOnlyProperties {
+    /**
+     * White-list of non-pblic fields allowed to be accessed by __get().
+     * Ignorered when it's empty (null or empty arrray), always checked otherwise.
+     * 
+     * @var array|null
+     */
+    protected static $VisibleFields;
+
+    /**
+     * "Magical" getter to access all non-public properties.
+     * 
+     * @return mixed
+     */
+    public function __get(string $name) {
+        if (empty($VisibleFields)) {
+            return property_exists($this, $name) ? $this->$name : null;
+        } else {
+            return in_array($name, self::$VisibleFields) ? $this->$name : null;
         }
     }
 }
